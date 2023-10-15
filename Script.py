1e390dd0f8dc3330d5a38c737d2773836b68948a
Uriel-PP <kaattoo9@gmail.com>
2023-10-14 18:10:32 -0600

    AutomataEnteros

--- Script.py
+++ Script.py
@@ -1,29 +1,41 @@
 class Automata:
     def __init__(self):
-        
-        self.estados = {'s0', 's1', 's2'}
-        
-        
-        self.simbolos = {'a', 'b', 'c'}
-        
+        self.estados = {'s1', 's8', 's9', 's10', 's11', 's12'}
+        self.simbolos = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+        self.punto = '.'
+        self.negativo = '-'
         
         self.transiciones = {
-            's0': {'a': 's1', 'b': 's0','c':''},
-            's1': {'a': 's1', 'b': 's2','c':'s1'},
-            's2': {'a': 's1', 'b': 's0','c':'s1'},
+            's1': {**{symbol: 's8' for symbol in self.simbolos}, self.negativo: 's10'},
+            's8': {**{symbol: 's8' for symbol in self.simbolos}, self.punto: 's9'},
+            's9': {**{symbol: 's9' for symbol in self.simbolos}},
+            's10': {**{symbol: 's11' for symbol in self.simbolos}},
+            's11': {**{symbol: 's11' for symbol in self.simbolos}, self.punto: 's12'},
+            's12': {**{symbol: 's12' for symbol in self.simbolos}},
         }
         
-        
-        self.estado_actual = 's0'
+        self.estado_actual = 's1'
     
     def procesar_cadena(self, cadena):
         for simbolo in cadena:
-            if simbolo not in self.simbolos:
+            if simbolo not in self.simbolos and simbolo != self.punto and simbolo != self.negativo:
                 return "Cadena rechazada"
             self.estado_actual = self.transiciones[self.estado_actual][simbolo]
         
-        if self.estado_actual == 's1':
-            return "Cadena aceptada"
+        if self.estado_actual in {'s8', 's9', 's11', 's12'}:
+            if self.estado_actual == 's8':
+                self.estado_actual = 's1'
+                return 'Numero entero'
+            elif self.estado_actual == 's9':
+                self.estado_actual = 's1'
+                return 'Decimal'
+            elif self.estado_actual == 's11':
+                self.estado_actual = 's1'
+                return 'Negativo Entero'
+            elif self.estado_actual == 's12':
+                self.estado_actual = 's1'
+                return 'Negativo Decimal'
+            self.estado_actual = 's1'
         else:
             return "Cadena rechazada"
 
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from Script import Automata
 
 automata = Automata()  
 
-
-cadena = input('Ingresa cadena: ')
-resultado = automata.procesar_cadena(cadena)
-print('-----------------------')
-print(resultado)
+while (True):
+    cadena = input('Ingresa cadena: ')
+    resultado = automata.procesar_cadena(cadena)
+    print('-----------------------')
+    print(resultado)
