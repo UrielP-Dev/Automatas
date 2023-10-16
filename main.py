abbe8870f8648ab08fc76f9dff0e8987ba77b534
Uriel-PP <kaattoo9@gmail.com>
2023-10-15 21:43:07 -0600

    Automatas ready

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ automata = Automata()
 
 while (True):
     cadena = input('Ingresa cadena: ')
-    resultado = automata.procesar_cadena(cadena)
+    resultado = automata.procesar_cadena(cadena,1)
     print('-----------------------')
     print(resultado)
